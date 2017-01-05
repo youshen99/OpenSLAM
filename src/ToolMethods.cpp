8671c71c2a33119c477d08051917bce995f3f1e0
youshen <727204747@qq.com>
2017-01-05 18:55:37 +0800

    工具方法封装

---
+++ src/ToolMethods.cpp
@@ -0,0 +1,83 @@
+#include "ToolMethods.h"
+namespace YOUSHEN_SLAM
+{
+   void ToolMethods::isImageToGrat(cv::Mat&image,cv::Mat&gray){
+	if(image.type()==CV_8UC1){
+       gray =image;
+	}else{
+       cvtColor(image,gray,CV_BGR2GRAY);
+	}
+}
+/**
+通过文件获取相机的内参和畸变函数
+*/
+void ToolMethods::getSettingFile(std::string settingPath,cv::Mat&mk,cv::Mat&mDistCoef){
+ cv::FileStorage fSettings(settingPath.c_str(), cv::FileStorage::READ);
+//路径无效的情况
+if(!fSettings.isOpened()){
+      std::cout << "Failed to open settings file at: " << settingPath << std::endl;
+      exit(-1);
+}
+//读取对应的参数文件
+ float fx = fSettings["Camera.fx"];
+    float fy = fSettings["Camera.fy"];
+    float cx = fSettings["Camera.cx"];
+    float cy = fSettings["Camera.cy"];
+
+    //     |fx  0   cx|
+    // K = |0   fy  cy|
+    //     |0   0   1 |
+    //构建相机内参矩阵
+    cv::Mat K = cv::Mat::eye(3,3,CV_32F);
+    K.at<float>(0,0) = fx;
+    K.at<float>(1,1) = fy;
+    K.at<float>(0,2) = cx;
+    K.at<float>(1,2) = cy;
+    K.copyTo(mk); 
+     // 图像矫正系数
+    //获取图像矫正系数
+    // [k1 k2 p1 p2 k3]
+    cv::Mat DistCoef(4,1,CV_32F);
+    DistCoef.at<float>(0) = fSettings["Camera.k1"];
+    DistCoef.at<float>(1) = fSettings["Camera.k2"];
+    DistCoef.at<float>(2) = fSettings["Camera.p1"];
+    DistCoef.at<float>(3) = fSettings["Camera.p2"];
+    const float k3 = fSettings["Camera.k3"];
+    if(k3!=0)
+    {
+        DistCoef.resize(5);
+        DistCoef.at<float>(4) = k3;
+    }
+    DistCoef.copyTo(mDistCoef);
+}
+
+//前者的長度比後者要大
+       void ToolMethods::computerMatch2(std::vector<cv::DMatch>&dMatch12,std::vector<cv::DMatch>&dMatch22,std::vector<cv::DMatch>&dMatchLaster2){
+               for (size_t i = 0; i < dMatch12.size(); i++)
+               {
+                    for (size_t j = 0; j < dMatch22.size(); j++)
+               {//判断对应的前后只是否是一样的
+                   if(dMatch12[i].queryIdx==dMatch22[j].trainIdx){
+                     if(dMatch22[j].queryIdx==dMatch12[i].trainIdx){
+                         dMatchLaster2.push_back(dMatch12[i]);       
+                        }
+                      }
+    
+                 }
+               }
+
+        }
+
+void ToolMethods::computerMatchPoint(std::vector<cv::DMatch>&dMatch1,std::vector<cv::DMatch>&dMatch2,std::vector<cv::DMatch>&dMatchLaster){
+     //计算输入的特征点的长度
+     if(dMatch1.size()<dMatch2.size()){
+       
+         computerMatch2(dMatch2,dMatch1,dMatchLaster);
+     }else{
+         computerMatch2(dMatch1,dMatch2,dMatchLaster);
+     }
+}
+
+
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 /*
  * @Author: youshen 
  * @Date: 2016-12-30 11:11:53 
- * @Last Modified by:   youshen 
- * @Last Modified time: 2016-12-30 11:11:53 
+ * @Last Modified by: youshen
+ * @Last Modified time: 2017-01-05 18:52:25
  */
 
 #include <iostream>
@@ -11,98 +11,17 @@
 #include "CommonAccessMethod.h"
 #include "Frame.h"
 #include "Binocular.h"
+#include "ToolMethods.h"
 using namespace std;
 using namespace cv;
 using namespace YOUSHEN_SLAM;
-//转换灰度图像 
-
-void isImageToGrat(Mat&image,Mat&gray){
-	if(image.type()==CV_8UC1){
-       gray =image;
-	}else{
-       cvtColor(image,gray,CV_BGR2GRAY);
-	}
-}
-/**
-通过文件获取相机的内参和畸变函数
-*/
-void getSettingFile(string settingPath,Mat&mk,Mat&mDistCoef){
- cv::FileStorage fSettings(settingPath.c_str(), cv::FileStorage::READ);
-//路径无效的情况
-if(!fSettings.isOpened()){
-      cerr << "Failed to open settings file at: " << settingPath << endl;
-      exit(-1);
-}
-//读取对应的参数文件
- float fx = fSettings["Camera.fx"];
-    float fy = fSettings["Camera.fy"];
-    float cx = fSettings["Camera.cx"];
-    float cy = fSettings["Camera.cy"];
-
-    //     |fx  0   cx|
-    // K = |0   fy  cy|
-    //     |0   0   1 |
-    //构建相机内参矩阵
-    cv::Mat K = cv::Mat::eye(3,3,CV_32F);
-    K.at<float>(0,0) = fx;
-    K.at<float>(1,1) = fy;
-    K.at<float>(0,2) = cx;
-    K.at<float>(1,2) = cy;
-    K.copyTo(mk); 
-     // 图像矫正系数
-    //获取图像矫正系数
-    // [k1 k2 p1 p2 k3]
-    cv::Mat DistCoef(4,1,CV_32F);
-    DistCoef.at<float>(0) = fSettings["Camera.k1"];
-    DistCoef.at<float>(1) = fSettings["Camera.k2"];
-    DistCoef.at<float>(2) = fSettings["Camera.p1"];
-    DistCoef.at<float>(3) = fSettings["Camera.p2"];
-    const float k3 = fSettings["Camera.k3"];
-    if(k3!=0)
-    {
-        DistCoef.resize(5);
-        DistCoef.at<float>(4) = k3;
-    }
-    DistCoef.copyTo(mDistCoef);
-}
-
-//前者的長度比後者要大
-       void computerMatch2(vector<DMatch>&dMatch12,vector<DMatch>&dMatch22,vector<DMatch>&dMatchLaster2){
-               for (size_t i = 0; i < dMatch12.size(); i++)
-               {
-                    for (size_t j = 0; j < dMatch22.size(); j++)
-               {//判断对应的前后只是否是一样的
-                   if(dMatch12[i].queryIdx==dMatch22[j].trainIdx){
-                     if(dMatch22[j].queryIdx==dMatch12[i].trainIdx){
-                         dMatchLaster2.push_back(dMatch12[i]);       
-                        }
-                      }
-    
-                 }
-               }
-
-        }
-
-void computerMatchPoint(vector<DMatch>&dMatch1,vector<DMatch>&dMatch2,vector<DMatch>&dMatchLaster){
-     //计算输入的特征点的长度
-     if(dMatch1.size()<dMatch2.size()){
-       
-         computerMatch2(dMatch2,dMatch1,dMatchLaster);
-     }else{
-         computerMatch2(dMatch1,dMatch2,dMatchLaster);
-     }
-}
-
 void testHanshu()
 {
   Mat image = imread("/home/glodon/imageShuang/left_image/000000.png");
   //相机矩阵  獲取相機的参数和畸变函数
   Mat K;
   Mat disCof;
-
-  getSettingFile("/home/glodon/MySLAM/ORB_SLAM2/Examples/Monocular/MYdATA.yaml",K,disCof);
-
-
+  YOUSHEN_SLAM::ToolMethods::getSettingFile("/home/glodon/MySLAM/ORB_SLAM2/Examples/Monocular/MYdATA.yaml",K,disCof);
   //第一张图片
   Frame *imageFrame = new Frame(image, K, disCof);
   //获取畸变矫正之后的点
@@ -124,7 +43,7 @@ void testHanshu()
    matcher.match(desc2, desc1, matches2);
 
 vector<DMatch> dMatchAChoosel;
-computerMatchPoint(matches2,matches,dMatchAChoosel);
+YOUSHEN_SLAM::ToolMethods::computerMatchPoint(matches2,matches,dMatchAChoosel);
   cout<<matches.size()<<"  "<<matches2.size()<<"    "<<dMatchAChoosel.size()<<endl;
   //初始化对应的函数
   CommonAccessMethod commonAN;
@@ -137,7 +56,8 @@ computerMatchPoint(matches2,matches,dMatchAChoosel);
   Mat F21;
   commonAN.findFundamental(keyPointList1,keyPointList2,dMatchAChoosel,vbMatchesInliersF,scoreF,F21,200,1);
   cout<<"H:  "<<scoreH<<"  F:  "<<scoreF<<endl;
-
+  float RH = scoreH/(scoreH+scoreF);
+  cout<<RH<<endl;
   //将获取到的点进行画到图像中
   Mat outImage;
   drawKeypoints(image, keyPointList1, outImage);
